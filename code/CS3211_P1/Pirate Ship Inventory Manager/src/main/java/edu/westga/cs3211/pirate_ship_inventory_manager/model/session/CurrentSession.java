10d26b51c4efb4e492270a9f6b5268feadc917f9
Matthew Shirley <mshirle5@my.westga.edu>
2025-12-07 01:15:00 -0500

    Fix more checkstyle warnings
    Fixed more checkstyle warnings after changing where checkstyle was applied.

--- code/CS3211_P1/Pirate Ship Inventory Manager/src/main/java/edu/westga/cs3211/pirate_ship_inventory_manager/model/session/CurrentSession.java
+++ code/CS3211_P1/Pirate Ship Inventory Manager/src/main/java/edu/westga/cs3211/pirate_ship_inventory_manager/model/session/CurrentSession.java
@@ -2,8 +2,20 @@ package edu.westga.cs3211.pirate_ship_inventory_manager.model.session;
 
 import edu.westga.cs3211.pirate_ship_inventory_manager.model.User;
 
+/**
+ * The CurrentSession class.
+ * 
+ * @author CS3211
+ * @version Fall 2025
+ */
 public class CurrentSession {
 	private User user;
+
+	/**
+	 * Initializes a new session with a user.
+	 * 
+	 * @param user the user
+	 */
 	public CurrentSession(User user) {
 		if (user == null) {
 			throw new IllegalArgumentException("User cannot be null");
@@ -11,16 +23,24 @@ public class CurrentSession {
 		this.user = user;
 	}
 
+	/**
+	 * Gets the user.
+	 * 
+	 * @return the user
+	 */
 	public User getUser() {
-		return user;
+		return this.user;
 	}
 
+	/**
+	 * Sets the user.
+	 * @param user the user
+	 */
 	public void setUser(User user) {
 		if (user == null) {
 			throw new IllegalArgumentException("User cannot be null");
 		}
 		this.user = user;
 	}
-	
-	
+
 }
--- code/CS3211_P1/Pirate Ship Inventory Manager/src/main/java/edu/westga/cs3211/pirate_ship_inventory_manager/viewModel/PickStorageWindowViewModel.java
+++ code/CS3211_P1/Pirate Ship Inventory Manager/src/main/java/edu/westga/cs3211/pirate_ship_inventory_manager/viewModel/PickStorageWindowViewModel.java
@@ -1,5 +1,10 @@
 package edu.westga.cs3211.pirate_ship_inventory_manager.viewModel;
 
+/**
+ * The PickStorageWindowViewModel class
+ * @version fall 2025
+ * @author CS3211
+ */
 public class PickStorageWindowViewModel extends SessionViewModel {
 	
 }
