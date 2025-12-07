03e42c41f68f3c7155f3f15922830fd180d23ba8
Matthew Shirley <mshirle5@my.westga.edu>
2025-12-07 00:27:47 -0500

    Fix checkstyle warnings
    Fixes two check style warnings

--- code/CS3211_P1/Pirate Ship Inventory Manager/src/main/java/edu/westga/cs3211/pirate_ship_inventory_manager/view/ReviewStockChangesWindow.java
+++ code/CS3211_P1/Pirate Ship Inventory Manager/src/main/java/edu/westga/cs3211/pirate_ship_inventory_manager/view/ReviewStockChangesWindow.java
@@ -3,7 +3,6 @@ package edu.westga.cs3211.pirate_ship_inventory_manager.view;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
-import java.util.Date;
 
 import edu.westga.cs3211.pirate_ship_inventory_manager.model.session.CurrentSession;
 import edu.westga.cs3211.pirate_ship_inventory_manager.viewModel.ReviewStockChangesViewModel;
@@ -32,11 +31,11 @@ public class ReviewStockChangesWindow implements SessionSetter {
 	@FXML
 	private ComboBox<String> chooseSortComboBox;
 
-    @FXML
-    private ListView<String> crewmateListView;
-    
-    @FXML
-    private DatePicker endDatePicker;
+	@FXML
+	private ListView<String> crewmateListView;
+
+	@FXML
+	private DatePicker endDatePicker;
 
 	@FXML
 	private Button filterButton;
@@ -56,167 +55,172 @@ public class ReviewStockChangesWindow implements SessionSetter {
 	@FXML
 	private AnchorPane pane;
 
-    @FXML
-    private DatePicker startDatePicker;
-    
-    private ReviewStockChangesViewModel reviewVM;
-    
-    @FXML
-    void initialize() {
-    	this.reviewVM = new ReviewStockChangesViewModel();
-    }
-    @FXML
-    void displayAllLogs(ActionEvent event) {
-    	this.changeResultsListView.getItems().setAll(
-    			FXCollections.observableArrayList(this.reviewVM.getLogChanges()));
-    }
+	@FXML
+	private DatePicker startDatePicker;
 
-    @FXML
-    void filterResultsListView(ActionEvent event) {
-    	if (this.chooseSortComboBox.getValue().equals("Crewmate")) {
-    		 ArrayList<String> selectedCrewmates = new ArrayList<>(this.crewmateListView.getSelectionModel().getSelectedItems());
-    	        
-    	        this.changeResultsListView.getItems().setAll(
-    	            FXCollections.observableArrayList(this.reviewVM.getCrewmateFilter(selectedCrewmates)));
-    	}
-    	if (this.chooseSortComboBox.getValue().equals("Special Quantity")) {
-	        this.changeResultsListView.getItems().setAll(
-	            FXCollections.observableArrayList(this.reviewVM.getSpecialQuantityFilter(this.reviewVM.getIsFlammableProperty().getValue(), this.reviewVM.getIsLiquidProperty().getValue(), this.reviewVM.getIsPerishableProperty().getValue())));
-    	}
-    	if (this.chooseSortComboBox.getValue().equals("Date") && this.endDatePicker.getValue() == null) {
-    		LocalDate startDate = this.startDatePicker.getValue();
-    		if (startDate != null) {
-    			DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MM/dd/yyyy");
-    			String startDateString = startDate.format(formatter);
-		        this.changeResultsListView.getItems().setAll(
-		            FXCollections.observableArrayList(this.reviewVM.getStartDateFilter(startDateString)));
-    		}
-    	}
-    	if (this.chooseSortComboBox.getValue().equals("Date") && this.endDatePicker.getValue() != null) {
-    	    LocalDate startDate = this.startDatePicker.getValue();
-    	    LocalDate endDate = this.endDatePicker.getValue();
-    	    if (endDate.isBefore(startDate)) {
-    	          this.displayErrorPopup("End date cannot be before start date");
-    	    } else {
-    	          DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MM/dd/yyyy");
-    	          String startDateString = startDate.format(formatter);
-    	          String endDateString = endDate.format(formatter);
-    	          this.changeResultsListView.getItems().setAll(
-    	              FXCollections.observableArrayList(this.reviewVM.getStartAndEndDateFilter(startDateString, endDateString)));
-    	        }
-    	}
-    }   
-    
-    private void displayErrorPopup(String message) { 
+	private ReviewStockChangesViewModel reviewVM;
+
+	@FXML
+	void initialize() {
+		this.reviewVM = new ReviewStockChangesViewModel();
+	}
+
+	@FXML
+	void displayAllLogs(ActionEvent event) {
+		this.changeResultsListView.getItems().setAll(FXCollections.observableArrayList(this.reviewVM.getLogChanges()));
+	}
+
+	@FXML
+	void filterResultsListView(ActionEvent event) {
+		if (this.chooseSortComboBox.getValue().equals("Crewmate")) {
+			ArrayList<String> selectedCrewmates = new ArrayList<>(
+					this.crewmateListView.getSelectionModel().getSelectedItems());
+
+			this.changeResultsListView.getItems()
+					.setAll(FXCollections.observableArrayList(this.reviewVM.getCrewmateFilter(selectedCrewmates)));
+		}
+		if (this.chooseSortComboBox.getValue().equals("Special Quantity")) {
+			this.changeResultsListView.getItems()
+					.setAll(FXCollections.observableArrayList(
+							this.reviewVM.getSpecialQuantityFilter(this.reviewVM.getIsFlammableProperty().getValue(),
+									this.reviewVM.getIsLiquidProperty().getValue(),
+									this.reviewVM.getIsPerishableProperty().getValue())));
+		}
+		if (this.chooseSortComboBox.getValue().equals("Date") && this.endDatePicker.getValue() == null) {
+			LocalDate startDate = this.startDatePicker.getValue();
+			if (startDate != null) {
+				DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MM/dd/yyyy");
+				String startDateString = startDate.format(formatter);
+				this.changeResultsListView.getItems()
+						.setAll(FXCollections.observableArrayList(this.reviewVM.getStartDateFilter(startDateString)));
+			}
+		}
+		if (this.chooseSortComboBox.getValue().equals("Date") && this.endDatePicker.getValue() != null) {
+			LocalDate startDate = this.startDatePicker.getValue();
+			LocalDate endDate = this.endDatePicker.getValue();
+			if (endDate.isBefore(startDate)) {
+				this.displayErrorPopup("End date cannot be before start date");
+			} else {
+				DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MM/dd/yyyy");
+				String startDateString = startDate.format(formatter);
+				String endDateString = endDate.format(formatter);
+				this.changeResultsListView.getItems().setAll(FXCollections
+						.observableArrayList(this.reviewVM.getStartAndEndDateFilter(startDateString, endDateString)));
+			}
+		}
+	}
+
+	private void displayErrorPopup(String message) {
 		Alert alert = new Alert(Alert.AlertType.ERROR);
 		alert.setContentText(message);
 		alert.showAndWait();
 	}
-    
-    private void setUpControls() {
-    	this.reviewVM = new ReviewStockChangesViewModel();
-    	String[] filters = {"Special Quantity", "Crewmate", "Date"};
-    	this.chooseSortComboBox.getItems().addAll(filters);
-    	this.changeResultsListView.getItems().addAll(this.reviewVM.getLogChanges());
-    	this.chooseSortComboBox.setValue(filters[0]);
-    	this.crewmateListView.getSelectionModel().setSelectionMode(SelectionMode.MULTIPLE);
-    	this.crewmateListView.getItems().addAll(this.reviewVM.getCrewList());
-    	this.startDatePicker.setEditable(false);
-    	this.endDatePicker.setEditable(false);
-    	
-        this.disableAllFilterControls();
-        
-        this.chooseSortComboBox.valueProperty().addListener((observable, oldValue, newValue) -> {
-            this.updateControlStates(newValue);
-        });
-        
-        this.startDatePicker.valueProperty().addListener((observable, oldValue, newValue) -> {
-        	boolean hasStartDate = newValue != null;
-        	this.endDatePicker.setDisable(!hasStartDate || !this.chooseSortComboBox.getValue().equals("Date"));
-        	
-        	if (newValue == null) {
-                this.endDatePicker.setValue(null);
-            }
-        });
-        
-        this.updateControlStates(this.chooseSortComboBox.getValue());
-    	
-    }
-    
-    private void disableAllFilterControls() {
-        this.isFlammableCheckBox.setDisable(true);
-        this.isLiquidCheckBox.setDisable(true);
-        this.isPerishableCheckBox.setDisable(true);
-        this.crewmateListView.setDisable(true);
-        this.startDatePicker.setDisable(true);
-        this.endDatePicker.setDisable(true);
-    }
 
-    private void updateControlStates(String selectedFilter) {
-        this.disableAllFilterControls();
-        
-        switch (selectedFilter) {
-            case "Special Quantity":
-                this.isFlammableCheckBox.setDisable(false);
-                this.isLiquidCheckBox.setDisable(false);
-                this.isPerishableCheckBox.setDisable(false);
-                break;
-                
-            case "Crewmate":
-                this.crewmateListView.setDisable(false);
-                break;
-                
-            case "Date":
-            	this.startDatePicker.setDisable(false);
-            	this.endDatePicker.setDisable(true);
+	private void setUpControls() {
+		this.reviewVM = new ReviewStockChangesViewModel();
+		String[] filters = { "Special Quantity", "Crewmate", "Date" };
+		this.chooseSortComboBox.getItems().addAll(filters);
+		this.changeResultsListView.getItems().addAll(this.reviewVM.getLogChanges());
+		this.chooseSortComboBox.setValue(filters[0]);
+		this.crewmateListView.getSelectionModel().setSelectionMode(SelectionMode.MULTIPLE);
+		this.crewmateListView.getItems().addAll(this.reviewVM.getCrewList());
+		this.startDatePicker.setEditable(false);
+		this.endDatePicker.setEditable(false);
 
-                break;
-                
-            default:
-                break;
-        }
-        
-        if (!selectedFilter.equals("Date")) {
-        	this.startDatePicker.setValue(null);
-        	this.endDatePicker.setValue(null);
-        }
-    }
-    
-    private void setUpBinds() {
-    	this.reviewVM.getIsFlammableProperty().bind(this.isFlammableCheckBox.selectedProperty());
-    	this.reviewVM.getIsLiquidProperty().bind(this.isLiquidCheckBox.selectedProperty());
-    	this.reviewVM.getIsPerishableProperty().bind(this.isPerishableCheckBox.selectedProperty());
-    }
-    
-       /**
-   	   * Provides bindings for the functionality
-   	   * 
-   	   */
-       public void bindToReviewStockChangesVM() {
-       	this.setUpControls();
-       	this.setUpBinds();
-       	this.displayAllLogsButton.setOnAction((event) -> {
-       		this.displayAllLogs(event);
-       	});
-       	this.filterButton.setOnAction((event) -> {
-       		this.filterResultsListView(event);
-       	});
-       }
-		private void initializeView() {
-			this.setUpControls();
-			this.setUpBinds();
-			this.displayAllLogsButton.setOnAction((event) -> {
-				this.displayAllLogs(event);
-			});
-			this.filterButton.setOnAction((event) -> {
-				this.filterResultsListView(event);
-			});
+		this.disableAllFilterControls();
 
+		this.chooseSortComboBox.valueProperty().addListener((observable, oldValue, newValue) -> {
+			this.updateControlStates(newValue);
+		});
+
+		this.startDatePicker.valueProperty().addListener((observable, oldValue, newValue) -> {
+			boolean hasStartDate = newValue != null;
+			this.endDatePicker.setDisable(!hasStartDate || !this.chooseSortComboBox.getValue().equals("Date"));
+
+			if (newValue == null) {
+				this.endDatePicker.setValue(null);
+			}
+		});
+
+		this.updateControlStates(this.chooseSortComboBox.getValue());
+
+	}
+
+	private void disableAllFilterControls() {
+		this.isFlammableCheckBox.setDisable(true);
+		this.isLiquidCheckBox.setDisable(true);
+		this.isPerishableCheckBox.setDisable(true);
+		this.crewmateListView.setDisable(true);
+		this.startDatePicker.setDisable(true);
+		this.endDatePicker.setDisable(true);
+	}
+
+	private void updateControlStates(String selectedFilter) {
+		this.disableAllFilterControls();
+
+		switch (selectedFilter) {
+		case "Special Quantity":
+			this.isFlammableCheckBox.setDisable(false);
+			this.isLiquidCheckBox.setDisable(false);
+			this.isPerishableCheckBox.setDisable(false);
+			break;
+
+		case "Crewmate":
+			this.crewmateListView.setDisable(false);
+			break;
+
+		case "Date":
+			this.startDatePicker.setDisable(false);
+			this.endDatePicker.setDisable(true);
+
+			break;
+
+		default:
+			break;
 		}
 
-		@Override
-		public void setSession(CurrentSession context) {
-			this.reviewVM.setCurrentSession(context);
-			this.initializeView();
+		if (!selectedFilter.equals("Date")) {
+			this.startDatePicker.setValue(null);
+			this.endDatePicker.setValue(null);
 		}
+	}
+
+	private void setUpBinds() {
+		this.reviewVM.getIsFlammableProperty().bind(this.isFlammableCheckBox.selectedProperty());
+		this.reviewVM.getIsLiquidProperty().bind(this.isLiquidCheckBox.selectedProperty());
+		this.reviewVM.getIsPerishableProperty().bind(this.isPerishableCheckBox.selectedProperty());
+	}
+
+	/**
+	 * Provides bindings for the functionality
+	 * 
+	 */
+	public void bindToReviewStockChangesVM() {
+		this.setUpControls();
+		this.setUpBinds();
+		this.displayAllLogsButton.setOnAction((event) -> {
+			this.displayAllLogs(event);
+		});
+		this.filterButton.setOnAction((event) -> {
+			this.filterResultsListView(event);
+		});
+	}
+
+	private void initializeView() {
+		this.setUpControls();
+		this.setUpBinds();
+		this.displayAllLogsButton.setOnAction((event) -> {
+			this.displayAllLogs(event);
+		});
+		this.filterButton.setOnAction((event) -> {
+			this.filterResultsListView(event);
+		});
+
+	}
+
+	@Override
+	public void setSession(CurrentSession context) {
+		this.reviewVM.setCurrentSession(context);
+		this.initializeView();
+	}
 }
